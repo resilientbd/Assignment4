0c6411b21a4dad162d89cb91af20449a3aef9dca
RIYAD Patwary <RIYAD Patwary@ROG_STRIX_G731GV>
2019-11-05 15:16:28 +0600

    assignment_4

---
+++ src/uylab_android_assignment_4/Assignment_4_Task_28.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uylab_android_assignment_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RIYAD Patwary
+ */
+public class Assignment_4_Task_28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        
+         */
+        //ask user to enter the value of 'S'
+        System.out.println("Enter the value of 'S' to find the value of L");
+        //initialize scanner
+        Scanner sc = new Scanner(System.in);
+        //declare variable and store value in to it
+        float myNum = sc.nextFloat();
+
+        if (myNum < 100) {
+            System.out.println("Value of L is " + (3000 - (125 * (myNum * myNum))));
+        } else if (myNum >= 100) {
+            System.out.println("Value of L is " + (12000 / (4 + ((myNum * myNum) / 14900))));
+        }
+    }
+
+}

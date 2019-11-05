c804041ddccc22445a01941cf89750a0f75d7586
Admin <Admin@DESKTOP-MRPLHGR>
2019-11-05 16:12:49 +0600

    Task 21 to 36 done

--- src/assignment4/Task35.java
+++ src/assignment4/Task35.java
@@ -5,40 +5,32 @@ import java.util.Scanner;
 public class Task35 {
 
     public static void main(String[] args) {
-    
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Time : ");
         int time = input.nextInt();
-        
-        if(time>=0&&time<=3){
+
+        if (time >= 0 && time <= 3) {
             System.out.println("Patience is a virtue");
-        }
-        else if(time>=4&&time<=6){
+        } else if (time >= 4 && time <= 6) {
             System.out.println("Breakfast");
-        }
-        else if(time>=7&&time<=11){
+        } else if (time >= 7 && time <= 11) {
             System.out.println("Patience is a virtue");
-        }
-        else if(time>=12&&time<=13){
+        } else if (time >= 12 && time <= 13) {
             System.out.println("Lunch");
-        }
-        else if(time>=14&&time<=15){
+        } else if (time >= 14 && time <= 15) {
             System.out.println("Patience is a virtue");
-        }
-        else if(time>=16&&time<=17){
+        } else if (time >= 16 && time <= 17) {
             System.out.println("Snacks");
-        }
-        else if(time==18){
+        } else if (time == 18) {
             System.out.println("Patience is a virtue");
-        }
-        else if(time>=19&&time<=20){
+        } else if (time >= 19 && time <= 20) {
             System.out.println("Dinner");
-        }
-        else if(time>=21&&time<=23){
+        } else if (time >= 21 && time <= 23) {
             System.out.println("Patience is a virtue");
-        }else{
+        } else {
             System.out.println("Wrong Time");
         }
     }
-    
+
 }
